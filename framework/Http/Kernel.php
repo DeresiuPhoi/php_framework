7354f6238df3af5778acb5e3529b23586db61ac8
nurlanMussepov <nurlanmusepov4@gmail.com>
2025-06-26 20:39:01 +0500

    Added Routes

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,19 +2,43 @@
 
 namespace Vendor\Framework\Http;
 
+use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 
 class Kernel
 {
+
     public function handle(Request $request): Response
     {
         $dispatcher = simpleDispatcher(function(RouteCollector $collector) {
-            $collector->addRoute("GET", "/", function () {
-                $content = "Hello World!";
+            $collector->get("/", function () {
+                $content = "Some content";
+                return new Response($content, 200, []);
+            });
+
+            $collector->get('/posts/{id:\d+}', function(array $vars){
+                $content = "<h1>Post - {$vars['id']}</h1>";
                 return new Response($content, 200, []);
             });
         });
-        dd($dispatcher);
+
+        $routeInfo = $dispatcher->dispatch(
+            $request->server['REQUEST_METHOD'],
+            $request->server['REQUEST_URI']
+        );
+
+        switch ($routeInfo[0]) {
+            case Dispatcher::NOT_FOUND:
+                return new Response("404 Not Found", 404, []);
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                return new Response("405 Method Not Allowed", 405, []);
+            case Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+                return $handler($vars);
+        }
+
+        return new Response("500 Internal Server Error", 500, []);
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+#вывод ошибок
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 use Vendor\Framework\Http\Kernel;
